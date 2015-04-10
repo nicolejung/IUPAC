82726d32354b9cb65525de35570e82f682dd13f9
unknown <abc@.(none)>
2015-04-10 13:05:59 +0200

    changes

--- lib/bracket.rb
+++ lib/bracket.rb
@@ -19,32 +19,13 @@ ob =Sample.new
 ob.Hello
 
 class Chemical_name < String
-  def find_position
 
-    # regular expression definition
-    all_pos=/(((?>\d|-|,|\s)*\d+)(?>\s|-)*\z|(-\(|\)|\{|\}|\[|\]-))/
-    single_pos=/^(?>\s|-|,)*(\d+)/
     
     #all_pos=/-\(|\)|\{|\}|\[|\]-/
-
+# 
+    # 4,4'-{1-[({5-[(4'-cyanobiphenyl-4-yl)oxy]pentyl}oxy)carbonyl]-2-[(4'-cyanobiphenyl-4-yl)oxy]ethylene}dibenzoic acid
     # match for position at the end of the chemical_name
-    if pos=self.match(all_pos)
-      ret=[$`]
-      pos[1].match(single_pos)
-    end
-
-    #if positions found, loop to extract each integer
-    while $1
-      n=$'
-      ret<<$1.to_i
-      n.match(single_pos)
-    end
-    # return the array, if ret is nil (no match) return [self]
-    ret || [self]
-  end
-
-end
-
+    
 #a=gets
 a=Chemical_name.new(gets.chomp)
 
